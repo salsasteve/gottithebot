2ea9ebb8f981c55b3cb8eadc9b5dedebb79c40f9
Elman Steve Laguna <32938596+salsasteve@users.noreply.github.com>
2022-12-27 23:21:11 -0600

    Update app.py
    Removing old commented code

--- app.py
+++ app.py
@@ -1,22 +1,3 @@
-# import sys
-# from chatgpt import Conversation
-
-# # Initialize the ChatGPT object with your API key
-# # model = ChatGPT(api_key="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")
-# conversation = Conversation(config_path="./config.json")
-
-# Stream the message as it arrives.
-# for chunk in conversation.stream("We are going to start a conversation. I will speak English and you will speak Portuguese."):
-#     print(chunk, end="")
-#     sys.stdout.flush()
-
-# # Wait until the message is fully received.
-# print(conversation.chat("What's the color of the sky?"))
-
-# # The AI will forget it was speaking Portuguese
-# conversation.reset()
-# print(conversation.chat("What's the color of the sun?"))
-
 import os
 import openai
 from dotenv import load_dotenv
